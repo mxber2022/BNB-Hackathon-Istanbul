bf612816b665a73b297ce306f72510ed4c3f37a6
mxber2022 <mxber2022@gmail.com>
2023-11-10 22:42:35 +0300

    frontend v1

---
+++ frontend/src/components/FetchTokenBound/Fetch.js
@@ -0,0 +1,16 @@
+import "./Fetch.css";
+import { useAccount, useConnect, useEnsName } from 'wagmi';
+import { ethers } from 'ethers';
+
+function Fetch () {
+    const { address, isConnected } = useAccount();
+    const { provider } = useConnect();
+
+    return(
+        <>
+
+        </> 
+    );
+}
+
+export default Fetch;
---
+++ frontend/src/components/Nav/Nav.js
@@ -0,0 +1,20 @@
+import "./Nav.css";
+function Nav () {
+    return(
+        <>
+            <nav className="nav">
+                <div>
+                    {
+                        <h1>ETH ISTANBUL</h1> 
+                    }
+                </div>
+
+                <div>
+                    <w3m-button />
+                </div>              
+            </nav>
+        </>
+    );
+}
+
+export default Nav;
